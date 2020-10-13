f6a35ca26464c1f549cf66f4a19c3b4729fba16e
Hugo <hugogseo@gmail.com>
2020-10-13 16:34:24 -0500

    Commit inicial

---
+++ config/yargs.js
@@ -0,0 +1,28 @@
+const argv = require('yargs')
+                .command('listar', 'imprime en consola la tabla de multiplicar',{
+                    base: {
+                        demand: true,
+                        alias: 'b'
+                    },
+                    limite: {
+                      alias:  'l',
+                    default: 10
+                     }
+                })
+                .command('crear', 'genera un archivo con la tabla de multiplicar',{
+                    base: {
+                        demand: true,
+                        alias: 'b'
+                    },
+                    limite: {
+                      alias:  'l',
+                    default: 10
+                     }
+                })
+                .help()
+                .argv;
+
+
+  module.exports = {
+      argv
+  }
